e7764ae0075ed2cc7473df8e62ac5a7499679475
as <adbudi1@gmail.com>
2021-06-27 14:11:16 +0700

    Tugas4 js
    dikumpulkan untuk niomic

---
+++ tugas4.js
@@ -0,0 +1,16 @@
+var  x = 60;
+var y = 81
+ var z = 72;
+function tinggiBadan(   ){
+  if (x > y) {
+  console.log('tinggi')
+  }
+  else if (y > z) {
+console.log('sedang')
+  }
+  else {
+    console.log('rendah')
+  }
+}
+tinggiBadan(   )
+console.log(x,y,z);
